9cf4671a9e6d8a5251aea38fa94afaad071198a1
MihailCCfit <MihailCCfit@users.noreply.github.com>
2022-10-09 05:44:37 +0000

    Deploying to gh-pages from @ MihailCCfit/oop@7964752f8ee43a0d9e72aa76ef53b04b4190c145 🚀

--- Task_1_2_2/type-search-index.js
+++ Task_1_2_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.fit.tsukanov","l":"Main"},{"p":"ru.nsu.fit.tsukanov","l":"Tree.Node"},{"p":"ru.nsu.fit.tsukanov","l":"Tree"},{"p":"ru.nsu.fit.tsukanov","l":"Tree.TreeIterBFS"},{"p":"ru.nsu.fit.tsukanov","l":"Tree.TreeIterDFS"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.fit.tsukanov","l":"Tree.Node"},{"p":"ru.nsu.fit.tsukanov","l":"Tree"},{"p":"ru.nsu.fit.tsukanov","l":"Tree.TreeIterBFS"},{"p":"ru.nsu.fit.tsukanov","l":"Tree.TreeIterDFS"}]
