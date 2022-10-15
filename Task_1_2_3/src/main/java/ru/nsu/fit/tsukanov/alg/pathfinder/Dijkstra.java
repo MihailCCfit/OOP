9b2dc37c971e3d25b4a0ec0de68bfd3e3bef5796
MihailCCfit <m.tsukanov@g.nsu.ru>
2022-10-15 18:58:32 +0700

    style fix

--- Task_1_2_3/src/main/java/ru/nsu/fit/tsukanov/alg/pathfinder/Dijkstra.java
+++ Task_1_2_3/src/main/java/ru/nsu/fit/tsukanov/alg/pathfinder/Dijkstra.java
@@ -12,26 +12,28 @@ import java.util.TreeMap;
 public class Dijkstra<V extends Comparable<V>, E> {
     private V startVert;
     private final TreeMap<V, Double> marksTree;
-    private final TreeMap<V, EdgeDefault<V,E>>  pathMap;
+    private final TreeMap<V, EdgeDefault<V, E>> pathMap;
     private final PriorityQueue<V> heap;
 
-    private final Graph<V,E> graph;
-    public Dijkstra(Graph<V,E> graph, V startVert){
+    private final Graph<V, E> graph;
+
+    public Dijkstra(Graph<V, E> graph, V startVert) {
         this.startVert = startVert;
         this.marksTree = new TreeMap<>();
-        this.heap = new PriorityQueue<>((v1,v2) -> (marksTree.get(v1).compareTo(marksTree.get(v2))));
+        this.heap = new PriorityQueue<>((v1, v2) -> (marksTree.get(v1).compareTo(marksTree.get(v2))));
         this.graph = graph;
         pathMap = new TreeMap<>();
         reuse();
     }
-    public void reuse(V start){
-        if (start==null){
+
+    public void reuse(V start) {
+        if (start == null) {
             throw new NullPointerException();
         }
-        if (!graph.containsVertex(start)){
+        if (!graph.containsVertex(start)) {
             throw new IllegalArgumentException("No such vertex in graph");
         }
-        this.startVert=start;
+        this.startVert = start;
         heap.clear();
         marksTree.clear();
         pathMap.clear();
@@ -39,22 +41,24 @@ public class Dijkstra<V extends Comparable<V>, E> {
         marksTree.put(startVert, 0.0);
         alg();
     }
-    public void reuse(){
+
+    public void reuse() {
         reuse(startVert);
     }
 
-    private void relax(EdgeDefault<V,E> edge){
+    private void relax(EdgeDefault<V, E> edge) {
         V start = edge.getEdgeSource();
         V end = edge.getEdgeTarget();
-        double res = marksTree.get(start)+ edge.getWeight();
-        if(!marksTree.containsKey(end) || res<marksTree.get(end)){
+        double res = marksTree.get(start) + edge.getWeight();
+        if (!marksTree.containsKey(end) || res < marksTree.get(end)) {
             marksTree.put(end, res);
             pathMap.put(end, edge);
             heap.add(end);
         }
     }
-    private void alg(){
-        while (!heap.isEmpty()){
+
+    private void alg() {
+        while (!heap.isEmpty()) {
             V curr = heap.poll();
             for (EdgeDefault<V, E> edge : graph.outgoingEdgesOf(curr)) {
                 relax(edge);
@@ -63,49 +67,50 @@ public class Dijkstra<V extends Comparable<V>, E> {
     }
 
 
-
-    public double getDistant(V v){
-        if (v==null){
+    public double getDistant(V v) {
+        if (v == null) {
             throw new NullPointerException("Null vertex");
         }
         return marksTree.getOrDefault(v, Double.POSITIVE_INFINITY);
     }
 
-    public List<V> getPathV(V v){
-        if (v==null){
+    public List<V> getPathV(V v) {
+        if (v == null) {
             throw new NullPointerException();
         }
-        if (!pathMap.containsKey(v)){
-            System.out.println(v+"|HE");
+        if (!pathMap.containsKey(v)) {
+            System.out.println(v + "|HE");
             return null;
         }
         LinkedList<V> list = new LinkedList<>();
         V curr = v;
         list.add(v);
-        while (curr!=startVert){
+        while (curr != startVert) {
             curr = pathMap.get(curr).getEdgeSource();
             list.add(0, curr);
         }
         return list;
     }
-    public List<EdgeDefault<V,E>> getPathE(V v){
-        if (v==null){
+
+    public List<EdgeDefault<V, E>> getPathE(V v) {
+        if (v == null) {
             return null;
         }
-        if (!pathMap.containsKey(v)){
+        if (!pathMap.containsKey(v)) {
             return null;
         }
-        LinkedList<EdgeDefault<V,E>> list = new LinkedList<>();
+        LinkedList<EdgeDefault<V, E>> list = new LinkedList<>();
         V curr = v;
 
-        while (curr!=startVert){
+        while (curr != startVert) {
             list.add(0, pathMap.get(curr));
             curr = pathMap.get(curr).getEdgeSource();
         }
         return list;
     }
-    public boolean hasPath(V v){
-        if (v==null) {
+
+    public boolean hasPath(V v) {
+        if (v == null) {
             return false;
         }
         return pathMap.containsKey(v);
