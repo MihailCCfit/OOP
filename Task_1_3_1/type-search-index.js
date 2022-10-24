9d7e0306ad16748cb7ab8cc6c1c284bfa775123f
MihailCCfit <MihailCCfit@users.noreply.github.com>
2022-10-24 06:57:42 +0000

    Deploying to gh-pages from @ MihailCCfit/oop@74a9cde61659c64244f0a3112c0e0a460e1cfadd 🚀

--- Task_1_3_1/type-search-index.js
+++ Task_1_3_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"substrings","l":"Finder"},{"p":"substrings.algorithms","l":"FindingsAK"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"substrings","l":"Finder"},{"p":"substrings.algorithms","l":"FinderAC"},{"p":"substrings.algorithms","l":"FinderAK"}]
