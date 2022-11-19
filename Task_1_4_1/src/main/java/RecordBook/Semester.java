1b5876390c61293f50f722fd3ece3080dd849b02
MihailCCfit <m.tsukanov@g.nsu.ru>
2022-11-19 23:52:08 +0700

    raw version

--- Task_1_4_1/src/main/java/RecordBook/Semester.java
+++ Task_1_4_1/src/main/java/RecordBook/Semester.java
@@ -1,25 +1,25 @@
 package RecordBook;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
 
 public class Semester {
-    private Map<String, Subject> subjectMap;
-    private int number;
+    private ArrayList<Subject> subjectMap;
+    private final int number;
 
     Semester(Collection<Subject> subjects, int number){
         this.number = number;
-        subjectMap = new HashMap<>();
-        for (Subject subject : subjects) {
-            subjectMap.put(subject.getSubjectName(), subject);
-        }
+        subjectMap = new ArrayList<>();
+        subjectMap.addAll(subjects);
+
     }
 
     public static void main(String[] args) {
-        nothing();
+        ///nothing();
+
     }
+    /*
     static private void nothing(){
         Subject subject = new Subject("Введение в алгебру и анализ", List.of("ОПК-1"),
                 "16.06.2022", "Экзамен",
@@ -33,4 +33,5 @@ public class Semester {
         Student me = new Student("Tsukanov", "Mikhail", "CCFIT", 21214, "m.tsukanov");
         System.out.println(me);
     }
+     */
 }
