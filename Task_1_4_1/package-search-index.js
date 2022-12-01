a75f6141857bc010a613a7fb20f10d55018491a3
MihailCCfit <MihailCCfit@users.noreply.github.com>
2022-12-01 10:15:46 +0000

    Deploying to gh-pages from @ MihailCCfit/oop@e2bb1a5abaacebd5c05d832dcb8ec69823739f51 🚀

--- Task_1_4_1/package-search-index.js
+++ Task_1_4_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.fit.tsukanov.recordBook.core"},{"l":"ru.nsu.fit.tsukanov.recordBook.ParserJSONstudentsData"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.fit.tsukanov.recordBook.core"},{"l":"ru.nsu.fit.tsukanov.recordBook.ParserJSONstudentsData"}];updateSearchResults();
