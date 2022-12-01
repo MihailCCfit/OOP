890ddce2a579f4593aa165e058ed03ce9db84108
MihailCCfit <m.tsukanov@g.nsu.ru>
2022-12-01 12:22:05 +0700

    starting implementing tablePrinter class

--- Task_1_4_1/src/main/java/ru/nsu/fit/tsukanov/recordBook/core/MarkType.java
+++ Task_1_4_1/src/main/java/ru/nsu/fit/tsukanov/recordBook/core/MarkType.java
@@ -11,36 +11,59 @@ public enum MarkType {
     GOOD("4"),
     EXCELLENT("5");
     public final String representation;
-    MarkType(String str){
+
+    MarkType(String str) {
         representation = str;
     }
 
-    public boolean isCreditMark(){
-        return switch (this){
+    public boolean isCreditMark() {
+        return switch (this) {
             case FAILED, PASSED -> true;
             default -> false;
         };
     }
-    long rawMark(){
-        return switch (this){
+
+    long rawMark() {
+        return switch (this) {
             case FAILED -> 0;
             case PASSED -> 1;
             case UNSATISFACTORY -> 2;
             case SATISFACTORY -> 3;
-            case GOOD ->  4;
+            case GOOD -> 4;
             case EXCELLENT -> 5;
         };
     }
 
     @Override
     public String toString() {
-        return switch (this){
+        return switch (this) {
             case FAILED -> "FAILED";
             case PASSED -> "PASSED";
             case UNSATISFACTORY -> "2";
             case SATISFACTORY -> "3";
-            case GOOD ->  "4";
+            case GOOD -> "4";
             case EXCELLENT -> "5";
         };
+    }
+
+    static public MarkType createMark(String inputString) {
+        MarkType mark = switch (inputString) {
+            case "PASSED" -> MarkType.PASSED;
+            case "FAILED" -> MarkType.FAILED;
+            case "2", "UNSATISFACTORY" -> UNSATISFACTORY;
+            case "3", "SATISFACTORY" -> SATISFACTORY;
+            case "4", "GOOD" -> GOOD;
+            case "5", "EXCELLENT" -> EXCELLENT;
+            default -> null;
+        };
+        if (mark == null) {
+            throw new IllegalArgumentException(inputString + "- illegal argument");
+        }
+        return mark;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MarkType.createMark("5"));
+        System.out.println(MarkType.createMark("PASSED"));
     }
 }
