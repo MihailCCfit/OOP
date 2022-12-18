26e831c0e8014edc6f32d2a56e77f0be632c4afc
MihailCCfit <m.tsukanov@g.nsu.ru>
2022-12-18 23:44:17 +0700

    add cos and sin

--- Task_1_5_1/src/main/java/ru/nsu/fit/tsukanov/calculator/complex/parsers/ComplexFunctionParser.java
+++ Task_1_5_1/src/main/java/ru/nsu/fit/tsukanov/calculator/complex/parsers/ComplexFunctionParser.java
@@ -5,6 +5,7 @@ import ru.nsu.fit.tsukanov.calculator.complex.Functions.ComplexAdd;
 import ru.nsu.fit.tsukanov.calculator.complex.Functions.ComplexDiv;
 import ru.nsu.fit.tsukanov.calculator.complex.Functions.ComplexMul;
 import ru.nsu.fit.tsukanov.calculator.complex.Functions.ComplexSub;
+import ru.nsu.fit.tsukanov.calculator.core.functions.Function;
 import ru.nsu.fit.tsukanov.calculator.core.parser.BuildFunctionParser;
 import ru.nsu.fit.tsukanov.calculator.core.parser.FunctionParser;
 
@@ -16,6 +17,46 @@ public class ComplexFunctionParser extends FunctionParser<ComplexNumber> {
                 .putFunction(List.of(new ComplexAdd(), new ComplexDiv()))
                 .putFunction(new ComplexMul())
                 .putFunction("-", new ComplexSub())
+                .putFunction("sin", new Function<>() {
+                    @Override
+                    public int getArity() {
+                        return 1;
+                    }
+
+                    @Override
+                    public ComplexNumber apply(ComplexNumber[] arguments) {
+                        ComplexNumber num = arguments[0];
+                        double real = num.getReal();
+                        double im = num.getImaginary();
+                        return new ComplexNumber(Math.sin(real) * Math.cosh(im),
+                                Math.cos(real) * Math.sinh(im));
+                    }
+
+                    @Override
+                    public String representation() {
+                        return "sin";
+                    }
+                })
+                .putFunction("cos", new Function<>() {
+                    @Override
+                    public int getArity() {
+                        return 1;
+                    }
+
+                    @Override
+                    public ComplexNumber apply(ComplexNumber[] arguments) {
+                        ComplexNumber num = arguments[0];
+                        double real = num.getReal();
+                        double im = num.getImaginary();
+                        return new ComplexNumber(Math.cos(real) * Math.cosh(im),
+                                -1 * Math.sin(real) * Math.sinh(im));
+                    }
+
+                    @Override
+                    public String representation() {
+                        return null;
+                    }
+                })
                 .build();
     }
 }
