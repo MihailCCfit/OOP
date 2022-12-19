cfaef8d14f236e0e480e7ea0062a3f542983b449
MihailCCfit <m.tsukanov@g.nsu.ru>
2022-12-19 21:13:03 +0700

    add dogs

--- Task_1_5_1/src/main/java/ru/nsu/fit/tsukanov/calculator/complex/ComplexNumber.java
+++ Task_1_5_1/src/main/java/ru/nsu/fit/tsukanov/calculator/complex/ComplexNumber.java
@@ -6,14 +6,30 @@ public class ComplexNumber {
     private final double real;
     private final double imaginary;
 
+    /**
+     * Return real part of complex number.
+     *
+     * @return real part of complex number
+     */
     public double getReal() {
         return real;
     }
 
+    /**
+     * Return imaginary part of complex number.
+     *
+     * @return imaginary part of complex number
+     */
     public double getImaginary() {
         return imaginary;
     }
 
+    /**
+     * Creates complex number by the two parts.
+     *
+     * @param real      the real part
+     * @param imaginary the imaginary part
+     */
     public ComplexNumber(double real, double imaginary) {
         this.real = real;
         this.imaginary = imaginary;
@@ -24,23 +40,51 @@ public class ComplexNumber {
         return "(" + real + "," + imaginary + ")";
     }
 
+    /**
+     * a+b. Sum of the complex numbers.
+     *
+     * @param a the first number
+     * @param b the second number
+     * @return the summary of numbers
+     */
     public static ComplexNumber add(ComplexNumber a, ComplexNumber b) {
         return new ComplexNumber(a.real + b.real, a.imaginary + b.imaginary);
     }
 
+    /**
+     * a-b. Subtraction of the complex numbers.
+     *
+     * @param a the first number
+     * @param b the second number
+     * @return the subtraction of numbers
+     */
     public static ComplexNumber sub(ComplexNumber a, ComplexNumber b) {
         return new ComplexNumber(a.real - b.real, a.imaginary - b.imaginary);
     }
 
-    public static ComplexNumber mul (ComplexNumber a, ComplexNumber b){
-        return new ComplexNumber(a.real*b.real - a.imaginary*b.imaginary,
-                a.real*b.imaginary + a.imaginary*b.real);
+    /**
+     * a*b. Multiplying of the complex numbers.
+     *
+     * @param a the first number
+     * @param b the second number
+     * @return the multiplying of numbers
+     */
+    public static ComplexNumber mul(ComplexNumber a, ComplexNumber b) {
+        return new ComplexNumber(a.real * b.real - a.imaginary * b.imaginary,
+                a.real * b.imaginary + a.imaginary * b.real);
     }
 
-    public static ComplexNumber div (ComplexNumber a, ComplexNumber b){
+    /**
+     * a/b. Multiplying of the complex numbers.
+     *
+     * @param a the first number
+     * @param b the second number
+     * @return the division of numbers
+     */
+    public static ComplexNumber div(ComplexNumber a, ComplexNumber b) {
         double koef = b.real * b.real + b.imaginary * b.imaginary;
-        double real = (a.real*b.real + a.imaginary*b.imaginary)/ koef;
-        double imag = (a.imaginary*b.real - a.real*b.imaginary)/ koef;
+        double real = (a.real * b.real + a.imaginary * b.imaginary) / koef;
+        double imag = (a.imaginary * b.real - a.real * b.imaginary) / koef;
         return new ComplexNumber(real, imag);
     }
 
@@ -52,8 +96,12 @@ public class ComplexNumber {
         return that.real == real && that.imaginary == imaginary;
     }
 
-    public double module(){
-        return Math.sqrt(real*real + imaginary*imaginary);
+    /**
+     * sqrt(real^2 + imaginary^2). Return the module of this complex number (distance from the centre).
+     * @return the module of this complex number
+     */
+    public double module() {
+        return Math.sqrt(real * real + imaginary * imaginary);
     }
 
     @Override
--- Task_1_5_1/src/main/java/ru/nsu/fit/tsukanov/calculator/core/parser/FunctionParser.java
+++ Task_1_5_1/src/main/java/ru/nsu/fit/tsukanov/calculator/core/parser/FunctionParser.java
@@ -6,23 +6,55 @@ import ru.nsu.fit.tsukanov.calculator.core.functions.Function;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Function parser. Parses by the map of name function to the function.
+ *
+ * @param <T> the number
+ */
 public class FunctionParser<T> {
     private final Map<String, Function<T>> dictionary = new HashMap<>();
 
-
+    /**
+     * Put new function for identification.
+     *
+     * @param function new function that will be added to the parser
+     * @return previous version
+     */
     public Function<T> putFunction(Function<T> function) {
         return putFunction(function.representation(), function);
     }
 
+    /**
+     * Put new function for identification.
+     *
+     * @param representation the string representation by that function will be recognized.
+     * @param function       new function that will be added to the parser
+     * @return previous version
+     */
     public Function<T> putFunction(String representation, Function<T> function) {
         return dictionary.put(representation, function);
     }
 
-    public boolean checkToken(String token){
+    /**
+     * Check token for the validity.
+     *
+     * @param token the string that will be checked for function.
+     * @return true if token can be recognized
+     */
+
+    public boolean checkToken(String token) {
         return dictionary.containsKey(token);
     }
+
+    /**
+     * Parse token to the function according to the saved function in the map.
+     *
+     * @param token the string(token) for parsing
+     * @return function according to the parser and token
+     * @throws BadLexemeException if there is problem with parsing
+     */
     public Function<T> parseToken(String token) throws BadLexemeException {
-        if (!checkToken(token)){
+        if (!checkToken(token)) {
             throw new BadLexemeException();
         }
         return dictionary.get(token);
