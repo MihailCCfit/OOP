40aa2d283b416af286428ed8c274da0e7d8fe955
MihailCCfit <m.tsukanov@g.nsu.ru>
2022-12-21 01:43:44 +0700

    small fix, remove google check

--- Task_1_5_1/src/main/java/ru/nsu/fit/tsukanov/calculator/template/example/StackCalculatorInstance.java
+++ Task_1_5_1/src/main/java/ru/nsu/fit/tsukanov/calculator/template/example/StackCalculatorInstance.java
@@ -7,6 +7,9 @@ import ru.nsu.fit.tsukanov.calculator.core.parser.CalculatorParser;
 import ru.nsu.fit.tsukanov.calculator.core.parser.Lexer;
 import ru.nsu.fit.tsukanov.calculator.template.stack.StackCalculatorTemplate;
 
+/**
+ * Prefix calculator for complex numbers.
+ */
 public class StackCalculatorInstance extends StackCalculatorTemplate<ComplexNumber> {
     public StackCalculatorInstance(CalculatorParser<ComplexNumber> calculatorParser, Lexer lexer) {
         super(calculatorParser, lexer);
