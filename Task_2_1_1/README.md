2505911783c394b4de2389d775ae8af7c8732b9a
MihailCCfit <m.tsukanov@g.nsu.ru>
2023-02-14 02:02:23 +0700

    png images in markdown Task2 with tests

---
+++ Task_2_1_1/README.md
@@ -0,0 +1,15 @@
+# Graphics
+
+## Разное кол-во создаваемых нитей
+
+![image](src\pythonPlot\png\DifferentThreads.png)
+
+## Разные виды
+
+![image](src\pythonPlot\png\DifferentTypes.png)
+
+## Разные виды с большими простыми числами
+
+![image](src\pythonPlot\png\DifferentTypesBigNumbers.png)
+Почему скорость линейного изменилась? Из-за того, что при большом кол-ве чисел
+используется преподсчёт простых делителей для чисел
--- Task_2_1_1/src/main/java/nsu/fit/tsukanov/parallel/prime/core/CheckerProvider.java
+++ Task_2_1_1/src/main/java/nsu/fit/tsukanov/parallel/prime/core/CheckerProvider.java
@@ -4,22 +4,6 @@ import java.util.Collection;
 
 public class CheckerProvider {
 
-    public static PrimeNumberChecker create(int maxNumber, int size) {
-        if (maxNumber < 0) {
-            throw new IllegalArgumentException("negative MaxNumber");
-        }
-        if (size < 0) {
-            throw new IllegalArgumentException("negative size");
-        }
-        double instant = size * (Math.sqrt(maxNumber + 1) + 2);
-        double preProc = Math.sqrt(maxNumber) * Math.log(Math.log(maxNumber + 2));
-        preProc += size * Math.sqrt(maxNumber) / (Math.log(maxNumber) + 1.08366);
-        if (instant < preProc) {
-            return new PrimeNumberCheckerInstant();
-        }
-        return new PrimeNumberCheckerWithPreprocessing(maxNumber);
-    }
-
     public static PrimeNumberChecker create(Collection<Integer> numbers) {
         return create(numbers, 1);
     }
@@ -42,21 +26,6 @@ public class CheckerProvider {
             return new PrimeNumberCheckerWithPreprocessing(max);
         }
         return new PrimeNumberCheckerInstant();
-    }
-
-    public static boolean isInstant(Collection<Integer> numbers) {
-        double instant = 0;
-        double preProc = 0;
-        int max = Integer.MIN_VALUE;
-        for (Integer number : numbers) {
-            if (number > max) {
-                max = number;
-            }
-            instant += Math.sqrt(number) + 2;
-            preProc += Math.sqrt(number) / (Math.log(number + 1) + 1) + 2;
-        }
-        preProc += Math.sqrt(max) * Math.log(Math.log(max + 2));
-        return instant < preProc;
     }
 
 }
