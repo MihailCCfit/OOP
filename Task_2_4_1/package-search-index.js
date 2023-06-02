0ab1bab695b192922f3c43e906d4d15461102cad
MihailCCfit <MihailCCfit@users.noreply.github.com>
2023-06-02 10:48:58 +0000

    Deploying to gh-pages from @ MihailCCfit/oop@2c6b3e08cb852ba09c8c0c4ae0cad11556d70229 🚀

---
+++ Task_2_4_1/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"nsu.fit.tsukanov"},{"l":"nsu.fit.tsukanov.dsl"},{"l":"nsu.fit.tsukanov.entity"},{"l":"nsu.fit.tsukanov.entity.common"},{"l":"nsu.fit.tsukanov.entity.group"},{"l":"nsu.fit.tsukanov.entity.tasks"},{"l":"nsu.fit.tsukanov.git"}];updateSearchResults();
